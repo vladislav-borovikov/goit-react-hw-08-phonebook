92384b1dd45dcdbf403dd8ba06cd591c12b2039d
vladislav-borovikov <vladislavborovikov24@gmail.com>
2022-05-14 15:28:49 +0300

    base link added

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/goit-react-hw-05-movies/">
           <App />
       </BrowserRouter>
     </Provider>   
